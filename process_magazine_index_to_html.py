b4f22dddade7d41a56d85995c40f90304c683127
Ryan G. Coleman <ryan.g.coleman@gmail.com>
2015-08-31 21:08:06 -0600

    notes

---
+++ process_magazine_index_to_html.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+#want at least 4 html outputs
+#chronological listing of every issue
+#chrono listing of every article
+#listing of every article in every category
+#listing of every article by every author
+#?????
